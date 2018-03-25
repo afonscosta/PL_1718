9aaeb0a88c8955250dbfd158aea8aa2b48f5fcd0
Afonso Costa <afonso.costa.15@gmail.com>
2018-03-25 12:49:34 +0100

    Segunda questão feita

--- CORPORA2/HP/cmd.sh
+++ CORPORA2/HP/cmd.sh
@@ -6,29 +6,27 @@ gawk -f extract_pers.awk ../harrypotter1 ../harrypotter2 > pers_freq_sort.csv
 
 # Percorre o dataset das biografias das personagens e produz um ficheiro
 # csv chamado pers_bio_sort_freq.csv com o formato: FREQUENCIA,NOME,BIOGRAFIA
-gawk -f add_bio.awk pers_freq_sort.csv characters.csv
+gawk -f add_bio.awk pers_freq_sort.csv characters.csv > pers_bio_sort_name.csv
 
-
-#Trata do plot
+#Extrai as personagens de cada livro individualmente para usar no plot
 gawk -f extract_pers.awk ../harrypotter1 > pers_freq_sort_hp1.csv
 gawk -f extract_pers.awk ../harrypotter2 > pers_freq_sort_hp2.csv
 
-
-#gawk -f swap_col.awk pers_freq_sort_hp1.csv > hp1.dat
-#gawk -f swap_col.awk pers_freq_sort_hp2.csv > hp2.dat
 #Cria o .dat para usar no plot
-gawk -f swap_col.awk pers_freq_sort_hp1.csv pers_freq_sort_hp2.csv > hp.dat
+gawk -f convert_data.awk pers_freq_sort_hp1.csv pers_freq_sort_hp2.csv > hp.dat
 gnuplot plot.dat
 
 # Percorre o ficheiro pers_bio_sort_freq.csv e produz o ficheiro latex
 # chamado lista_pers_harry_potter.tex em formato de lista
-gawk -f export_latex.awk pers_bio_sort_freq.csv
+gawk -f export_latex.awk pers_bio_sort_name.csv > lista_pers_harry_potter.tex
 
-#Produz pdf
+#Produz pdf final
 pdflatex lista_pers_harry_potter.tex &> /dev/null
+
+#Apaga ficheiros intermédios
 rm lista_pers_harry_potter.aux
 rm lista_pers_harry_potter.log
 rm lista_pers_harry_potter.out
-
-
-#gnuplot d1.dat
+rm lista_pers_harry_potter.toc
+rm pers_freq_sort_hp1.csv
+rm pers_freq_sort_hp2.csv
--- CORPORA2/HP/extract_pers.awk
+++ CORPORA2/HP/extract_pers.awk
@@ -1,6 +1,6 @@
 #!/usr/bin/gawk -f
 
-NF > 0 && NR >= 315 && $4 ~ "^NP.*" { 
+NF > 0 && $4 ~ "^NP.*" && length($2) > 2 { 
 	gsub(/_/, " ", $2); #Substitui o '_' por um espaço
 	personagens[$2]++
 } 
@@ -12,8 +12,4 @@ END {
 	for ( i in personagens )  {
 		print personagens[i] "," i
 	}
-
-	#system("sort -rn personagens_freq.csv > pers_freq_ord.csv && rm personagens_freq.csv")
-	#system("sort -k2 -t, -rn personagens_nome.csv > pers_nome_ord.csv && rm personagens_nome.csv")
-	#system("sed -e 's/,/ /g' pers_nome_ord.csv > pers_nome_ord.dat && rm pers_nome_ord.csv")
 }
