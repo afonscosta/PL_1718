49f4fdb30b62ce41b49774f48cd8cfa90192b428
Marco Silva <marant_silva@hotmail.com>
2018-05-03 10:36:58 +0100

    atualização

--- Parte2/lex.yy.c
+++ Parte2/lex.yy.c
@@ -360,8 +360,8 @@ static void yy_fatal_error (yyconst char msg[]  );
 	*yy_cp = '\0'; \
 	(yy_c_buf_p) = yy_cp;
 
-#define YY_NUM_RULES 3
-#define YY_END_OF_BUFFER 4
+#define YY_NUM_RULES 2
+#define YY_END_OF_BUFFER 3
 /* This struct is not used in this scanner,
    but its presence is necessary. */
 struct yy_trans_info
@@ -369,10 +369,10 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static yyconst flex_int16_t yy_accept[16] =
+static yyconst flex_int16_t yy_accept[12] =
     {   0,
-        0,    0,    0,    0,    4,    2,    3,    2,    2,    2,
-        2,    2,    1,    1,    0
+        0,    0,    0,    0,    3,    2,    2,    0,    0,    1,
+        0
     } ;
 
 static yyconst flex_int32_t yy_ec[256] =
@@ -412,32 +412,28 @@ static yyconst flex_int32_t yy_meta[5] =
         1,    2,    1,    1
     } ;
 
-static yyconst flex_int16_t yy_base[22] =
+static yyconst flex_int16_t yy_base[15] =
     {   0,
-       15,   14,    0,    0,   15,    0,   18,   11,    0,   10,
-        8,    8,    7,    0,   18,    0,    2,    4,    8,    7,
-        6
+        0,    8,    0,    0,    9,   11,    5,    4,    2,    0,
+       11,    0,    2,    4
     } ;
 
-static yyconst flex_int16_t yy_def[22] =
+static yyconst flex_int16_t yy_def[15] =
     {   0,
-       16,   17,   18,   18,   15,   19,   15,   20,   19,   20,
-       10,   21,   21,   13,    0,   15,   15,   15,   15,   15,
-       15
+       12,   13,   12,   12,   11,   11,   14,   14,    8,    8,
+        0,   11,   11,   11
     } ;
 
-static yyconst flex_int16_t yy_nxt[23] =
+static yyconst flex_int16_t yy_nxt[16] =
     {   0,
-        6,    6,    8,    8,    7,    7,   13,   10,    9,   14,
-       14,   12,   11,   11,   15,    7,    7,    5,   15,   15,
-       15,   15
+        6,    6,    7,    7,    8,   10,    9,    9,   11,    6,
+        5,   11,   11,   11,   11
     } ;
 
-static yyconst flex_int16_t yy_chk[23] =
+static yyconst flex_int16_t yy_chk[16] =
     {   0,
-       16,   16,   17,   17,   18,   18,   21,   20,   19,   13,
-       12,   11,   10,    8,    5,    2,    1,   15,   15,   15,
-       15,   15
+       12,   12,   13,   13,   14,    9,    8,    7,    5,    2,
+       11,   11,   11,   11,   11
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -466,7 +462,7 @@ char buffer[1000];
 char* replace(char* string, char* token);
 char *substring(char *string, int position, int length);
 
-#line 470 "lex.yy.c"
+#line 466 "lex.yy.c"
 
 #define INITIAL 0
 #define dic 1
@@ -655,7 +651,7 @@ YY_DECL
 #line 14 "marktex.l"
 
 
-#line 659 "lex.yy.c"
+#line 655 "lex.yy.c"
 
 	if ( !(yy_init) )
 		{
@@ -709,13 +705,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 16 )
+				if ( yy_current_state >= 12 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 18 );
+		while ( yy_base[yy_current_state] != 11 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -750,15 +746,10 @@ YY_RULE_SETUP
 	YY_BREAK
 case 2:
 YY_RULE_SETUP
-#line 22 "marktex.l"
-;
-	YY_BREAK
-case 3:
-YY_RULE_SETUP
-#line 24 "marktex.l"
+#line 23 "marktex.l"
 ECHO;
 	YY_BREAK
-#line 762 "lex.yy.c"
+#line 753 "lex.yy.c"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(dic):
 	yyterminate();
@@ -1052,7 +1043,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 16 )
+			if ( yy_current_state >= 12 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
@@ -1080,11 +1071,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 16 )
+		if ( yy_current_state >= 12 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 15);
+	yy_is_jam = (yy_current_state == 11);
 
 	return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1759,7 +1750,7 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 24 "marktex.l"
+#line 23 "marktex.l"
 
 
 
@@ -1800,25 +1791,21 @@ char *substring(char *string, int position, int length)
 
 char* replace(char* string, char* token){
 
-	char *f, *e;
-	int length = strlen(string);
+	char *f;
 	char aux[10000];
 	strcpy(aux, string);
 
 	for (int i = 0; i < string[i] != '\0'; i++){
 		if (string[i] == token[0] && string[i + 1] == token[1]){
 			f = substring(string, 1, i );
-			e = substring(string, i + 3, length - i -2);
 			//printf("%s - - %s\n",f, e);
 			
 			strcpy(aux, "");
-			//sprintf(string, "\\textbf{%s}", f);
-			strcat(aux, f);
+			sprintf(aux, "\\textbf{%s}", f);
+			//strcat(aux, f);
 			free(f);
 
 			strcat(aux, "$\\bullet$\\");
-			strcat(aux, e);
-			free(e);
 		}
 	}
 	return strdup(aux);
